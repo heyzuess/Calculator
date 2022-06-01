396fc90060b341d28017f754ceae36d6455999fd
Jesse Iberri <jesseiberri@gmail.com>
2022-06-01 12:40:29 -0700

    Added key input + fixed some found issues.

--- script.js
+++ script.js
@@ -47,6 +47,8 @@ class Calculator {
     evaluate () {
         this.result = Calculator.action(this.action, this.value1, this.value2);
 
+        if (!this.result) return;
+
         /* This keeps the result to a fixed size so it doesnt stretch calculator
            a full stop character takes up two px with current font, hence the +2.
            
@@ -84,13 +86,34 @@ const ogLimit = myCalculator.maxDisplay;
 let resizeVal = .25;
 let resizeStep = .25;
 
+document.addEventListener('keydown', keyboardInput);
+
+/* Add ids to number keys */
+let numbers = Array.from(options);
+numbers.forEach(option => {
+    let temp = parseInt(option.innerText);
+    if (!temp) return;
+    option.id = temp.toString();
+});
+
 let actionUpdate = (ev, action) => {
+    // Resize font will return  1 if resize needed and possible
+    //             will return  0 if resize not needed
+    //             will return -1 if resize needed and not possible
+    //
+    // in which case we ignore further input if its not an action
+    // or second input
+    if (resizeFont() < 0 && 
+        !myCalculator.actions.includes(action) &&
+        !myCalculator.action) return;
+
     if (!myCalculator.action && myCalculator.actions.includes(action)) {
         myCalculator.value1 = parseFloat(display.innerText);
         myCalculator.action = action;
         myCalculator.init = true;
 
-        ev.target.classList.add('active');
+        if (!ev.target.classList.contains('special'))
+            ev.target.classList.add('active');
 
         return;
     }
@@ -105,7 +128,9 @@ let actionUpdate = (ev, action) => {
         display.innerText = myCalculator.result;
 
         clearActive();
-        ev.target.classList.add('active');
+
+        if (!ev.target.classList.contains('special'))
+            ev.target.classList.add('active');
 
         return;
     }
@@ -113,20 +138,6 @@ let actionUpdate = (ev, action) => {
     if (myCalculator.init) {
         display.innerText = '';
         myCalculator.init = false;
-    }
-
-    /* Will resize text on screen to allow more input */ 
-    if (display.innerText.length + 1 > myCalculator.maxDisplay) {
-        let currentSize = parseInt(ogFontSize.replaceAll('px'));
-        let newSize = parseInt(currentSize - (currentSize * resizeVal));
-
-        if (newSize <= 0) return;
-         
-        let newLimit = parseInt(ogLimit * (1 / (1 - resizeVal)));
-        resizeVal = Math.min(resizeVal + resizeStep, 1);
-
-        display.style.fontSize = `${newSize}px`;
-        myCalculator.maxDisplay = newLimit;
     }
 
     display.innerText += action;
@@ -142,18 +153,11 @@ for (let option of options) {
     option.addEventListener('click', (ev) => actionUpdate(ev, option.innerText));
 }
 
-actions.clear.addEventListener('click', (ev) => {
-    myCalculator.clear();
-    clearActive();
-    display.innerText = myCalculator.result;
-
-    /* Reseting original size values */ 
-    display.style.fontSize = ogFontSize;
-    myCalculator.maxDisplay = ogLimit;
-    resizeVal = resizeStep;
-});
+actions.clear.addEventListener('click', clear);
 
 actions.equals.addEventListener('click', () => {
+    if (!myCalculator.value1) return;
+    
     myCalculator.value2 = parseFloat(display.innerText);
     myCalculator.evaluate();
     myCalculator.value1 = myCalculator.result;
@@ -178,4 +182,90 @@ actions.plusminus.addEventListener('click', () => {
 function clearActive () {
     let activeActions = Array.from(document.querySelectorAll('.action.active'));
     if (activeActions) activeActions.forEach(action => action.classList.remove('active'));
+}
+
+function backspace () {
+    let disp = display.innerText;
+    let temp = Array.from(disp);
+
+    if (temp.length > 1) {
+        temp.pop();
+        disp = temp.join('');
+    } else {
+        disp = '0';
+        myCalculator.init = true;
+    }
+
+    display.innerText = disp;
+}
+
+function clear() {
+    myCalculator.clear();
+    clearActive();
+    display.innerText = myCalculator.result;
+
+    /* Reseting original size values */ 
+    display.style.fontSize = ogFontSize;
+    myCalculator.maxDisplay = ogLimit;
+    resizeVal = resizeStep;
+}
+
+function keyboardInput (ev) {
+    let temp = parseInt(ev.key);
+
+    if (isNaN(temp)) {
+        switch(ev.key) {
+            case 'Backspace':
+                backspace();
+                break;
+            case 'Escape':
+                clear();
+                break;
+            case '+':
+                actions.add.dispatchEvent(new Event('click'));
+                break;
+            case '-':
+                actions.subtract.dispatchEvent(new Event('click'));
+                break;
+            case '/':
+                actions.divide.dispatchEvent(new Event('click'));
+                break;
+            case '*':
+                actions.multiply.dispatchEvent(new Event('click'));
+                break;
+            case '%':
+                actions.modulo.dispatchEvent(new Event('click'));
+                break;
+            case '=':
+            case 'Enter':
+                actions.equals.dispatchEvent(new Event('click'));
+                break;
+            default:
+                // If not handled then ignore key input
+                console.log(`unhandled key: ${ev.key}`);
+                break;
+        }
+        return;
+    } else {
+        let option = document.getElementById(temp.toString());
+        option.dispatchEvent(new Event('click'));
+    }
+}
+
+function resizeFont() {
+    /* Will resize text on screen to allow more input */ 
+    if (display.innerText.length + 1 > myCalculator.maxDisplay) {
+        let currentSize = parseInt(ogFontSize.replaceAll('px'));
+        let newSize = parseInt(currentSize - (currentSize * resizeVal));
+
+        if (newSize <= 0) return -1;
+         
+        let newLimit = parseInt(ogLimit * (1 / (1 - resizeVal)));
+        resizeVal = Math.min(resizeVal + resizeStep, 1);
+
+        display.style.fontSize = `${newSize}px`;
+        myCalculator.maxDisplay = newLimit;
+        return 1;
+    }
+    return 0;
 }
